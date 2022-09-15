018e5298dccc68b70a3e3fc997b7119b05f7535a
Edson Bollis <edsonbollis@gmail.com>
2022-09-14 23:13:08 -0300

    Update states_cities.dart

--- states_cities.dart
+++ states_cities.dart
@@ -1,6 +1,6 @@
 const List<String> states = <String>['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF',
   'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ',
-  'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP'];
+  'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 
 const Map<String, List> cities = {
   'AC': [
